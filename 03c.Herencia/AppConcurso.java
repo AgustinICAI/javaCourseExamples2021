ec1b9a16407cc307ed74fff5a70bbd30348ea8ec
b0904853 <agustin_gomez_lamela@carrefour.com>
2021-02-11 09:12:50 +0100

    clase a medias

--- 03c.Herencia/AppConcurso.java
+++ 03c.Herencia/AppConcurso.java
@@ -2,26 +2,35 @@ public class AppConcurso
 {
   public static void main (String argv[])
   {
+    Concursante [] concunsantes = new Concursante[30];
+    
+    
     //Hago upcasting para poder agrupar funcionalidad de todos mis concursantse
-    Concursante c1 = new Casado("Lola", 29, "femenino");
-    Concursante c2 = new Casado("Diego", 30, "masculino");
-    Concursante s1 = new Soltero("Manuel",28, "masculino");
+    concunsantes[0] = new Casado("Lola", 29, "femenino");
+    concunsantes[1] = new Casado("Diego", 30, "masculino");
+    concunsantes[2] = new Soltero("Manuel",28, "masculino");
         
     //Hago downcasting para acceder a métodos de un objeto hijo
-    ((Casado)c1).setParejaActual(c2);
-    ((Casado)c2).setParejaActual(c1);
+    ((Casado)concunsantes[0]).setParejaActual(c2);
+    ((Casado)concunsantes[1]).setParejaActual(c1);
     
     
-    System.out.println(c1);
-    System.out.println(c2);
-    System.out.println(s1);
+    for(Concursante c : concursantes)
+    {
+      System.out.println (c);
     
-    ((Casado)c1).pedirCita(s1);
+    }
+    
+    //((Casado)c1).pedirCita(s1);
     
     System.out.println(c1.getEstancia());
     System.out.println(c2.getEstancia());
     System.out.println(s1.getEstancia());
     
+    
+    
+    
+        
     
     
     

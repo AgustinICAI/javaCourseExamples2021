51befefa0ca8c1a8250ec6d36268ffad5189d97b
b0904853 <agustin_gomez_lamela@carrefour.com>
2021-03-26 09:01:58 +0100

    subiendo ventana basica

--- 08c1.swing/JVentana.java
+++ 08c1.swing/JVentana.java
@@ -1,54 +1,59 @@
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
 
 public class JVentana extends JFrame
 {
-    public static void main(String args[])
+    public static void main(String argv[])
     {
         new JVentana();
     }
-
     public JVentana()
     {
-        super("Mi titulo");
-        this.setLayout(new BorderLayout());
+        super("Formulario ICAI");
+        //POR DEFECTO EL LAYOUT DE UN JFRAME ES UN BORDERLAYOUT
+        JPanel pnlSouth = new JPanel();
+        pnlSouth.setBackground(Color.YELLOW);
+        this.add(pnlSouth,BorderLayout.SOUTH);
+        JButton jbtn1 = new JButton("ENVIAR");
+        //this.add(jbtn1,BorderLayout.SOUTH);
+        pnlSouth.add(jbtn1);
 
-        JPanel pnlCentro = new JPanel();
-        pnlCentro.setLayout(new GridLayout(2,2));
-    
-        JLabel lblNombre = new JLabel("Nombre:");
-        JLabel lblEdad = new JLabel("Edad:");
-        JTextField txtNombre = new JTextField(10);
-        JTextField txtEdad = new JTextField(2);
+        JPanel pnlNorth = new JPanel();
+        pnlNorth.setBackground(new Color(187, 9, 9));
+        this.add(pnlNorth,BorderLayout.NORTH);
 
-        pnlCentro.add(lblNombre);
-        pnlCentro.add(txtNombre);
-        pnlCentro.add(lblEdad);
-        pnlCentro.add(txtEdad);
-        
-        JPanel pnlSur = new JPanel();
-        pnlSur.setLayout(new FlowLayout());
+        JPanel pnlWest = new JPanel();
+        pnlWest.setBackground(Color.BLUE);
+        this.add(pnlWest,BorderLayout.WEST);
 
-        JButton btnGuardar = new JButton("Guardar");
-        pnlSur.add(btnGuardar);
+        JPanel pnlEast = new JPanel();
+        pnlEast.setBackground(Color.GREEN);
+        this.add(pnlEast,BorderLayout.EAST);
 
-        this.add(pnlCentro, BorderLayout.CENTER);
-        this.add(pnlSur, BorderLayout.SOUTH);
-        
-        //this.setTitle("Un titulo");
-        //this.setSize(400, 200);
-        this.pack(); //Ajusta el tamaño de la ventana al tamaño de sus componentes.
-        this.setLocation(500, 10);
-        
-        this.setVisible(true); //Último método
+        JPanel pnlCenter = new JPanel();
+        this.add(pnlCenter,BorderLayout.CENTER);
+        JLabel jlb1 = new JLabel("Nombre");
+        JLabel jlb2 = new JLabel("Apellido1");
+        JLabel jlb3 = new JLabel("Apellido2");
+        JTextField jtxt1 = new JTextField();
+        JTextField jtxt2 = new JTextField();
+        JTextField jtxt3 = new JTextField();
+        //EL LAYOUT POR DEFECTO DE UN PANEL ES UN FLOWLAYOUT
+        pnlCenter.setLayout(new GridLayout(3,2));
+        pnlCenter.add(jlb1);
+        pnlCenter.add(jtxt1);
+        pnlCenter.add(jlb2);
+        pnlCenter.add(jtxt2);
+        pnlCenter.add(jlb3);
+        pnlCenter.add(jtxt3);
+
+        //this.setSize(500,500);
+        this.pack();
+        //this.setResizable(false);
+        //this.setLocation(300,300);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
 

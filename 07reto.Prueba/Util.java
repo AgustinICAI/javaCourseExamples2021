eb6d55e78b59c3662b39fd82bdd1c827d858d994
b0904853 <agustin_gomez_lamela@carrefour.com>
2021-04-11 19:05:07 +0200

    subiendo codigo

---
+++ 07reto.Prueba/Util.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Util {
+    static HashMap<Integer,Integer> EncontrarPareja(int[] A, int sum) {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+    }
+
+    public static HashMap<Integer, Integer> EscribirParejasSimetricas(HashMap<Integer,Integer> hm) {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+    }
+
+
+    public static boolean TieneTodosNumConsecutivos(int[] b) {
+    /* Código a desarrollar
+    *
+    *
+    *
+    * */
+    }
+}
